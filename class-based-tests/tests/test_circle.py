245bb868d8a33504c31d5ba1d2b06b6b5fdf54f0
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2025-05-22 11:03:21 +0600

    Class based tests added

---
+++ class-based-tests/tests/test_circle.py
@@ -0,0 +1,28 @@
+import math
+
+import pytest
+import source.shapes as shapes
+
+class TestCircle:
+
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        self.circle = shapes.Circle(10)
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        del self.circle
+
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_one(self):
+        assert True
+
+    def test_two(self):
+        assert True
+
+    def perimeter(self):
+        result = self.circle.perimeter()
+        expected_result = 2 * math.pi * self.circle.radius
+        assert result == expected_result
